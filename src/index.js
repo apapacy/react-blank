459b252cc4ee4f11587bf4d478e383db2a1ee765
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-03-01 21:22:19 +0200

    modal popup+

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ var $ = require('jquery');
 var jQuery = $;
 var foundation = require("foundation")
 var React = require('react');
-var layout = require('./react/Layout.jsx');
-React.render(
-	React.createElement(layout),
+var ReactDOM = require('react-dom');
+var LoginForm = require('./react/admin/LoginForm.jsx');
+ReactDOM.render(
+	React.createElement(LoginForm),
 	document.getElementById('layout')
 );
 
 
 function Open() {
-  $('#myModal').foundation('reveal', 'open');
+  alert("open")
+  $('#LoginForm').foundation('reveal', 'open');
 }
 
 function Close() {
-  $('#myModal').foundation('reveal', 'close');
+  alert("close")
+  $('#LoginForm').foundation('reveal', 'close');
 }
 
 setTimeout(Close, 5000)
 
 setTimeout(Open, 10000)
 
-setTimeout(Close, 15000)
+setTimeout(Close, 20000)
--- src/react/admin/LoginForm.jsx
+++ src/react/admin/LoginForm.jsx
@@ -4,22 +4,26 @@ var React = require('react');
 class LoginForm extends React.Component {
   render() {
     return (
-<div id="myModal" className="reveal-modal">
-  <form>
+  <div id="LoginForm" className="reveal-modal reveal tiny " data-reveal>
     <div className="row">
-      <div className="medium-6 columns">
-        <label>User Name
-          <input type="text" placeholder=".medium-6.columns" />
-        </label>
-      </div>
-      <div className="medium-6 columns">
-        <label>Secret Password
-          <input type="password" placeholder=".medium-6.columns" />
-        </label>
+      <div className="medium-8 medium-centered large-8 large-centered small-8 small-centered columns ">
+        <form>
+          <div className="row column log-in-form">
+            <h4 className="text-center">Log in with you email account</h4>
+            <label>Email
+              <input type="text" placeholder="somebody@example.com" />
+            </label>
+            <label>Password
+              <input type="text" placeholder="Password" />
+            </label>
+            <input id="show-password" type="checkbox" /><label htmlFor="show-password">Show password</label>
+            <p><a type="submit" className="button expanded">Log In</a></p>
+            <p className="text-center"><a href="#">Forgot your password?</a></p>
+          </div>
+        </form>
       </div>
     </div>
-  </form>
-</div>
+  </div>
     )
   }
 }
